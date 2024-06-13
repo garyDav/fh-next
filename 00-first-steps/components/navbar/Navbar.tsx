8ac22ae33849d0f71377be467431635cf923c0a9
garyDav <gary.2810.dav@gmail.com>
2024-06-13 18:26:51 -0400

    Navbar

--- 00-first-steps/components/navbar/Navbar.tsx
+++ 00-first-steps/components/navbar/Navbar.tsx
@@ -1,19 +1,29 @@
+import Link from 'next/link'
+
+const navItems = [
+  { path: '/about', text: 'About' },
+  { path: '/pricing', text: 'Pricing' },
+  { path: '/contact', text: 'Contact' },
+]
+
 export const Navbar = () => {
   return (
     <nav className='flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded'>
-      <span>Home</span>
+      <Link href='/'>
+        <span>Home</span>
+      </Link>
 
       <div className='flex flex-1'></div>
 
-      <a className='mr-2' href='/about'>
+      <Link className='mr-2' href='/about'>
         About
-      </a>
-      <a className='mr-2' href='/pricing'>
+      </Link>
+      <Link className='mr-2' href='/pricing'>
         Pricing
-      </a>
-      <a className='mr-2' href='/contact'>
+      </Link>
+      <Link className='mr-2' href='/contact'>
         Contact
-      </a>
+      </Link>
     </nav>
   )
 }
